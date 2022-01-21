7b1d318b110018985a4a22acc1df70953b60ba2c
rlahoua7 <2152116@talnet.nl>
2022-01-21 12:21:16 +0100

    opdracht 3

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,19 +1,28 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE rank = 7;
 -- Opdracht 2 
+SELECT * FROM jaar2016 WHERE country = "Nederland";
 
 -- Opdracht 3
+SELECT * FROM jaar2015 WHERE country = "Nederland";
 
 -- Opdracht 4
+SELECT * FROM jaar2016 WHERE country = "Nederland";
 
 -- Opdracht 5
+SELECT * FROM jaar2015 WHERE region = "North america";
 
 -- Opdracht 6 
+SELECT * FROM jaar2016 WHERE rank = 25;
 
 -- Opdracht 7 
+SELECT * FROM jaar2016 WHERE score > 7087;
 
 -- Opdracht 8
+SELECT * FROM jaar2016 WHERE country, regio <= 5;
 
 -- Opdracht 9
+SELECT * FROM jaar2015 WHERE country, score = "ireland";
 
--- Opdracht 10
+-- Opdracht 10 
+SELECT * FROM jaar2016 WHERE region = "Latin America and Caribbean";
