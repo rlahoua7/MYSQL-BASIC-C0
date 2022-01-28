5bc5a1a35981fda76db7ec298f59de5b55bae9fd
rlahoua7 <2152116@talnet.nl>
2022-01-28 11:20:56 +0100

    opdracht 3

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,28 +1,25 @@
 -- Opdracht 1 
-SELECT * FROM jaar2015 WHERE rank = 7;
+SELECT * FROM jaar2015 WHERE rank=7;
 -- Opdracht 2 
-SELECT * FROM jaar2016 WHERE country = "Nederland";
+SELECT * FROM jaar2016  WHERE rank=7;
 
 -- Opdracht 3
-SELECT * FROM jaar2015 WHERE country = "Nederland";
+SELECT * FROM jaar2015 score WHERE country="Netherlands";
 
 -- Opdracht 4
-SELECT * FROM jaar2016 WHERE country = "Nederland";
-
+SELECT * FROM jaar2016 score WHERE country="Netherlands";
 -- Opdracht 5
-SELECT country FROM jaar2015 WHERE region = "North america";
-
+SELECT * FROM jaar2015 country WHERE region ="North America";
 -- Opdracht 6 
-SELECT country FROM jaar2016 WHERE rank = 25;
+SELECT country FROM jaar2016 WHERE rank=25;
 
 -- Opdracht 7 
-SELECT country, region FROM jaar2016 WHERE score > 7087;
+SELECT country FROM jaar2016 WHERE score > 7087;
 
 -- Opdracht 8
-SELECT * FROM jaar2016 WHERE country, regio <= 5;
+SELECT country, region FROM jaar2016 WHERE rank <=5;
 
 -- Opdracht 9
-SELECT country, score FROM jaar2015 WHERE country = "ireland";
-
--- Opdracht 10 
+SELECT country, score FROM jaar2015 WHERE country="Ireland";
+-- Opdracht 10
 SELECT country FROM jaar2016 WHERE region = "Latin America and Caribbean";
